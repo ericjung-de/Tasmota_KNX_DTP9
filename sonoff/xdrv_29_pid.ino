748eb8b8c258300ddc84d5a45690913dd31e45d5
Adrian Scillato <35405447+ascillato@users.noreply.github.com>
2019-10-18 10:48:14 -0300

    .

--- sonoff/xdrv_29_pid.ino
+++ sonoff/xdrv_29_pid.ino
@@ -1,5 +1,5 @@
 /*
-  xdrv_29_pid.ino - PID Controller Algorithm Support for Sonoff-Tasmota
+  xdrv_30_pid.ino - PID Controller Algorithm Support for Sonoff-Tasmota
 
   Copyright (C) 2018  Colin Law, Thomas Herrmann and Adrian Scillato
 
@@ -370,9 +370,9 @@ static void run_pid()
  * Interface
 \*********************************************************************************************/
 
-#define XDRV_29
+#define XDRV_30
 
-boolean Xdrv29(byte function)
+boolean Xdrv30(byte function)
 {
   boolean result = false;
 
